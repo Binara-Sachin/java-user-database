7030a143753e171316e8e041a8b86877bc9c8ee4
Binara Sachin <binara.sachin@gmail.com>
2021-09-24 20:41:27 +0530

    first commit

---
+++ src/main/java/com/binarasachin/jud/App.java
@@ -0,0 +1,13 @@
+package com.binarasachin.jud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
